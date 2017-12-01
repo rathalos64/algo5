06d255cb3142b4126a16b57f0417029d8a1dd0b9
Alen Kocaj <alen.kocaj@posteo.at>
2017-12-01 16:12:54 +0100

    [03] ToDos

--- 03_kmeans_gmm/gmm/gmm.py
+++ 03_kmeans_gmm/gmm/gmm.py
@@ -4,11 +4,22 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.stats import multivariate_normal
+
 from functools import reduce
     
 from mpl_toolkits.mplot3d import Axes3D
 
 from sklearn.datasets.samples_generator import make_blobs
+
+# TODO:
+# * compare multivariate pdf with scipy.stats's implementation
+#   The probability densities are the same although the scaling differs.
+#   For my implementation, based on some mu and cov, the max p differes up to
+#   0.06 to numpy's impelementation. This affects the log-likelihood
+#   Me (-5032.44494116788) vs Scipy (-3384.526508165725).
+#   [NOTE]: This only concerns the scaling factor. The kernel function was implemented
+#   correctly.
 
 def main():
     mean = np.array([0, 0])
@@ -28,11 +39,17 @@ def main():
     densities = np.array([multivariate_normal_pdf(sample, mean, cov) for sample in samples])
 
     print(_loglikelihood(samples, mean, cov))
-    return
+    # print(_loglikelihood_numpy(samples, mean, cov))
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(samples.T[0], samples.T[1], densities, linestyle="None", marker=".", depthshade=True)
+    ax.scatter(samples.T[0], samples.T[1], densities, linestyle="None", c="g", marker=".", depthshade=True)
+
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(111, projection='3d')
+    densies = multivariate_normal.pdf(samples, mean=mean, cov=cov)
+    ax2.scatter(samples.T[0], samples.T[1], densies, linestyle="None", c="r", marker=".", depthshade=True)
+
     plt.show()
 
 # multivariate_normal_pdf calculates the density of a given observation x based
@@ -50,7 +67,11 @@ def multivariate_normal_pdf(x, mean, cov):
 # _loglikelihood calculate the logarithmic likelihood of data X given the parameters
 # of a multivariate normal distribution (mean and covariance matrix)
 def _loglikelihood(X, mean, cov):
-    return reduce(lambda acc, x: acc + math.log(multivariate_pdf(x, mean, cov)), X, 0)
+    return reduce(lambda acc, x: acc + math.log(multivariate_normal_pdf(x, mean, cov)), X, 0)
+
+def _loglikelihood_numpy(X, mean, cov):
+    Y = multivariate_normal.pdf(X, mean=mean, cov=cov)
+    return reduce(lambda acc, x: acc + math.log(x), Y, 0)
 
 if __name__ == "__main__":
     main()
